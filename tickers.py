eccef49121123db933b66d2562cc742145c5d750
Julian Sweatt <20158985+juliansweatt@users.noreply.github.com>
2019-03-25 20:26:51 -0400

    Output & Ticker Filtering
    - Added Output to File Specified by System Argument
    - Added Filtering (Only Output First n Valid Tickers)

--- tickers.py
+++ tickers.py
@@ -2,6 +2,7 @@
 import sys
 import requests
 import re
+from iex import Stock
 
 NASDAQ_URL = "http://www.nasdaq.com/screening/companies-by-industry.aspx?exchange=NASDAQrender=download"
 
@@ -9,7 +10,26 @@ def save_tickers(num, outfile):
     response = requests.get(NASDAQ_URL)
     tickers = set(re.findall(r'symbol/([a-z]*)', response.text))
 
+    validTickers = set()
+
+    for ticker in tickers:
+        try:
+            Stock(ticker).price()
+            validTickers.add(ticker)
+        except:
+            print("Invalid Ticker", ticker)
+    
+    f = open(outfile, "w")
+
+    for i,ticker in enumerate(validTickers):
+        if i < num:
+            f.write(ticker + "\n")
+        else:
+            break
+
+    f.close()
+
 if __name__ == '__main__':
-    numTickers = sys.argv[1]
+    numTickers = int(sys.argv[1])
     outFileName = sys.argv[2]
     save_tickers(numTickers, outFileName)
