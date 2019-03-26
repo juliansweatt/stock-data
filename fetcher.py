303c7a94beaa535eac4fdfc8c571c132ef883f8c
Julian Sweatt <20158985+juliansweatt@users.noreply.github.com>
2019-03-25 21:40:13 -0400

    Added Time Loop
    Added concept for time-based updating.

--- fetcher.py
+++ fetcher.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 import sys
+import time
+import datetime
 
 def getTickers(fName):
     tickers = set()
@@ -11,11 +13,25 @@ def getTickers(fName):
         f.close()
     except:
         print("Error While Accessing the Ticker File.")
+        return
 
     return tickers
 
 if __name__ == '__main__':
+    # Collect System Arguments
     timeLimit = int(sys.argv[1])
     tickerFname = sys.argv[2]
     infoFname = sys.argv[3]
+
+    # Collect Stock Tickers
     print(getTickers(tickerFname)) # Printing for debugging
+
+    # Timed Execution
+    endTime = time.time() + timeLimit
+    currentMinute = datetime.datetime.now().minute
+    while time.time() < endTime:
+        if(datetime.datetime.now().minute != currentMinute):
+            print("Updating Stock Data")
+            # Execute Update Here
+            currentMinute = datetime.datetime.now().minute
+    print("Time Limit Has Expired at", datetime.datetime.now().time())
