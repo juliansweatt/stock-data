1c6f354a3fe5fabfa617c50d81731284f92e6c51
Julian Sweatt <20158985+juliansweatt@users.noreply.github.com>
2019-03-25 21:15:07 -0400

    Added getTickers() Method
    Added `getTickers(fName)` method which access the `fName` file and saves each line as a ticker in a new set object which is returned.

--- fetcher.py
+++ fetcher.py
@@ -1,7 +1,21 @@
 #!/usr/bin/env python3
 import sys
 
+def getTickers(fName):
+    tickers = set()
+
+    try:
+        f = open(fName,"r")
+        for line in f:
+            tickers.add(line[:-1])
+        f.close()
+    except:
+        print("Error While Accessing the Ticker File.")
+
+    return tickers
+
 if __name__ == '__main__':
     timeLimit = int(sys.argv[1])
     tickerFname = sys.argv[2]
     infoFname = sys.argv[3]
+    print(getTickers(tickerFname)) # Printing for debugging
