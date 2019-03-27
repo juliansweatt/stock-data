e1a93135e2202cf7369161cbfbd3a1614c9cc02c
hlh16b <47165266+hlh16b@users.noreply.github.com>
2019-03-27 04:46:11 -0400

    Stock query implementation
    Added the stock query function to be triggered every minute and to print in the format for a comma delimited csv file.

--- fetcher.py
+++ fetcher.py
@@ -2,6 +2,7 @@
 import sys
 import time
 import datetime
+from iex import Stock
 
 def getTickers(fName):
     tickers = set()
@@ -24,7 +25,12 @@ if __name__ == '__main__':
     infoFname = sys.argv[3]
 
     # Collect Stock Tickers
-    print(getTickers(tickerFname)) # Printing for debugging
+    tickerSet = getTickers(tickerFname)
+    print(tickerSet)  # Printing for debugging
+
+    #setup for print file
+    outfile = open(infoFname, "w")
+    outfile.write("Time, Ticker, latestPrice, latestVolume, Close, Open, low, high\n")
 
     # Timed Execution
     endTime = time.time() + timeLimit
@@ -33,5 +39,12 @@ if __name__ == '__main__':
         if(datetime.datetime.now().minute != currentMinute):
             print("Updating Stock Data")
             # Execute Update Here
+            for ticker in tickerSet:
+                tickInfo = Stock(ticker).quote()
+                tickStr = str(datetime.datetime.now().hour) + ":" + str(currentMinute) + ", " + ticker + ", " + str(tickInfo["latestPrice"]) + ", "
+                tickStr = tickStr + str(tickInfo["latestVolume"]) + ", " + str(tickInfo["close"]) + ", " + str(tickInfo["open"]) + ", "
+                tickStr = tickStr + str(tickInfo["low"]) + ", " + str(tickInfo["high"]) + "\n"
+                outfile.write(tickStr)
             currentMinute = datetime.datetime.now().minute
     print("Time Limit Has Expired at", datetime.datetime.now().time())
+    outfile.close()
